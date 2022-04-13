ebce3d3afc25949d2db6a7e1fc6aee8094a7b75f
PhatDave <kosmodiskclassic0@gmail.com>
2022-04-13 13:29:42 +0200

    Initial commit

---
+++ src/main/java/hr/cyka/filehosting/FilehostingApplication.java
@@ -0,0 +1,13 @@
+package hr.cyka.filehosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilehostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilehostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/cyka/filehosting/FilehostingApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.cyka.filehosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilehostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
